f492efcddc164eebbcf8c43f10fb3074e1f94fdb
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-08-19 01:36:37 +0200

    Add the skeleton for fronend

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  isCold: boolean = true;
+  bodyClass: string = ""
+  containerClass: string = ""
+
+  ngOnInit(): void {
+    this.simulateFetchTemperature(); 
+    console.log("init called")
+  }
+
+  simulateFetchTemperature(): void {
+    const simulatedTemperature = 9;
+    this.isCold = simulatedTemperature < 10;
+    console.log("simulate called")
+    this.updateClasses();
+  }
+
+  updateClasses(): void {
+    this.bodyClass = this.isCold ? 'cold-body' : 'hot-body';
+    this.containerClass = this.isCold ? 'cold-container' : 'hot-container';
+
+    console.log("update called")
+  }
 }
